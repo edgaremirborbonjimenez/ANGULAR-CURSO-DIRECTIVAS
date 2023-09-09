dfcc550c185eee45cca8034d4800490061c5dc57
SnakeEater007 <edemboji@gmail.com>
2023-09-09 09:57:06 -0700

    Arreglando cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directivaApp';
+  title = 'Registro de Usuarios';
+  nombre:string='';
+  apellido:string='';
+  mensaje:string='';
+  cargo:string='';
+  registrado=false;
+  datos;
+  
+  constructor(){
+
+    this.datos = [
+  
+      {text:"Java"},
+  
+      {text:"JavaScript"},
+  
+      {text:"Python"},
+  
+      {text:"C++"},
+
+    ];
+
+  }
+
+
+seRegistroUsuario(){
+  this.registrado=true;
+
+  this.mensaje='Se a registrado un nuevo usuario'
+}
+
+
 }
